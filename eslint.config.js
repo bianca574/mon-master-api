1ca790946a4e382c5cdbd2b110a1ff8bd3647fb2
Bianca-Ioana Ilie-Serban <bianca-ioana.ilie-serban@etu.u-paris.fr>
2026-08-12 15:31:24 +0300

    Fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -12,4 +12,18 @@ module.exports = [
       },
     },
   },
+  {
+    files: ['tests/**/*.test.js'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+      },
+    },
+  },
 ];
